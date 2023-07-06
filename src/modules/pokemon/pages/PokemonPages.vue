57e9cdcde0305d3b1bad31e7090f759f4cd82dea
David-Masabanda <josue.masabanda99@gmail.com>
2023-07-06 11:18:26 -0500

    Deber 10

--- src/modules/pokemon/pages/PokemonPages.vue
+++ src/modules/pokemon/pages/PokemonPages.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="game">
-    <h1 v-if="!pokemonCorrecto">Espere por favor...</h1>
-    <div v-else>
+    <h1 class="smsCarga" v-if="!pokemonCorrecto">Espere por favor...</h1>
+    <div v-else class="estructura">
       <h1>Juego Pokemon</h1>
-
-      <PokemonImg
-        :pokemonId="pokemonCorrecto.id"
-        :muestraPokemon="showPokemon"
-      />
+      <label class="puntos" for="">Puntaje: {{ score }}</label>
+      <PokemonImg :pokemonId="pokemonCorrecto.id" :muestraPokemon="showPokemon" />
 
       <PokemonOps :opciones="arreglo" @seleccionar="revisarSeleccion($event)" />
-      <Button @click="reset">Reiniciar</Button>
-      <label for="">Puntaje: {{score}}</label>
+
+      <Button v-show="intentos == 3 || acertoPokemon" @click="reset">Reiniciar</Button>
+      <label for="">Intento: {{ intentos }}</label> <br>
+
+      <label class="nombreCorrecto" v-show="intentos == 3 && error" for="">El Pokemon era: {{ pokemonCorrecto.nombre
+      }}</label><br>
+
+      <label v-show="error" class="erroneo" for="">El juego ha terminado, inténtalo nuevamente</label><br>
+      <label v-show="acertoPokemon" class="acertado" for="">Felicitaciones, ¡has ganado!</label>
     </div>
+
+
   </div>
 </template>
 
@@ -29,7 +35,11 @@ export default {
       arreglo: [],
       pokemonCorrecto: null,
       showPokemon: false,
-      score:0
+      score: 0,
+      intentos: 0,
+      acertoPokemon: false,
+      error: false
+
     };
   },
   components: {
@@ -46,20 +56,33 @@ export default {
     },
 
     revisarSeleccion(idSeleccionado) {
-      console.log("Evento padre");
-
+      this.intentos++;
       console.log(idSeleccionado);
       if (idSeleccionado == this.pokemonCorrecto.id) {
         this.showPokemon = true;
-        this.score++
-      } else {
+        if (this.intentos === 1) {
+          this.score += 5;
+        } else if (this.intentos === 2) {
+          this.score += 2;
+        } else if (this.intentos === 3) {
+          this.score += 1;
+        }
+        this.acertoPokemon = true;
+      } else if (this.intentos === 3 && this.score === 0) {
         this.showPokemon = false;
+        this.error = true
       }
     },
     reset() {
       this.arreglo = [];
       this.pokemonCorrecto = null;
       this.showPokemon = false;
+      this.intentos = 0;
+      this.acertoPokemon = false;
+      this.score = 0;
+      this.intentos = 0;
+      this.acertoPokemon = false;
+      this.error = false;
       this.cargaJuegoInicial()
     },
   },
@@ -73,13 +96,49 @@ export default {
 
 <style scoped>
 .game {
-  display: grid;
+  display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  text-align: center;
 }
+
 h1 {
-  color: aqua;
+  color: rgb(220, 124, 83);
   margin-bottom: 10px;
   width: 100%;
+  justify-content: center;
+}
+
+.smsCarga {
+  color: black;
+}
+
+.estructura {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.puntos {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.erroneo {
+  color: rgb(221, 92, 92);
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.acertado {
+  color: rgb(147, 147, 232);
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.nombreCorrecto {
+  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 15px;
 }
 </style>
